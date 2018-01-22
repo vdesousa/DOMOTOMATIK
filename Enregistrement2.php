8643c97548248edd4dc749a4f60ebdaf7ef53e57
Virgilio de Sousa <34256309+vdesousa@users.noreply.github.com>
2018-01-22 16:54:35 +0100

    rename erreur

--- Enregistrement2.php
+++ Enregistrement2.php
@@ -3,25 +3,30 @@
 		<span>Créer mon profil</span>
 		<form method="post" action="index2.php">
 			<label>Nom:
-			<input type="text" name="nom" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="invalidenoms") {echo "<p>Veuillez utiliser que des lettres</p>";}?><br>
+			<input type="text" name="nom" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="invalidenoms") {echo "<span class=erreur>Veuillez utiliser que des lettres</span>";}?><br>
 			<label>Prénom:
-			<input type="text" name="prenom" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="invalidenoms") {echo "<p>Veuillez utiliser que des lettres</p>";}?><br>
+			<input type="text" name="prenom" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="invalidenoms") {echo "<span class=erreur>Veuillez utiliser que des lettres</span>";}?><br>
 			<label>Numéro de téléphone:
-			<input type="text" name="numero" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="invalidetel") {echo "<p>Veuillez utiliser que des chiffres</p>";}?><br>
+			<input type="text" name="numero" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="invalidetel") {echo "<span class=erreur>Veuillez utiliser que des chiffres</span>";}?><br>
 			<label>Mot de Passe:
 			<input type="password" name="password" /></label><br>
 			<label>Confirmer mot de passe:
-			<input type="password" name="passwordconfirmation" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="mdp") {echo "<p>Les mots de passe ne correspondent pas</p>";}?><br>
+			<input type="password" name="passwordconfirmation" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="mdp") {echo "<span class=erreur>Les mots de passe ne correspondent pas</span>";}?><br>
 			<label>Adresse:
-			<input type="text" name="adresse" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="adr") {echo "<p>Adresse invalide</p>";}?><br>
+			<input type="text" name="adresse" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="adr") {echo "<span class=erreur>Adresse invalide</span>";}?><br>
 			<label>Code Postal:
-			<input type="text" name="codepostal" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="cp") {echo "<p>Code Postal invalide</p>";}?><br>
+			<input type="text" name="codepostal" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="cp") {echo "<span class=erreur>Code Postal invalide</span>";}?><br>
 			<label>Ville:
-			<input type="text" name="ville" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="vil") {echo "<p>Ville invalide</p>";}?><br>
-			<input type="checkbox" name="CGU" />J'accepte les <a href="CGU.php">CGU</a><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="CGU") {echo "<p>Veuillez accepter les CGU</p>";}?><br>
+			<input type="text" name="ville" /></label><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="vil") {echo "<span class=erreur>Ville invalide</span>";}?><br>
+			<input type="checkbox" name="CGU" />J'accepte les <a href="CGU.php">CGU</a><?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="CGU") {echo "<span class=erreur>Veuillez accepter les CGU</span>";}?><br>
 			<input type="checkbox" name="Newsletter" />Je m'inscris à la Newsletter<br>
 			<input type="submit" name="submit" value="Continuer" /><br>
-			<?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="vide") {echo "<p>Veuillez remplir tous les champs</p>";}?>
+			<?php if (isset($_GET["enregistrement"]) AND $_GET["enregistrement"]=="vide") {echo "<span class=erreur>Veuillez remplir tous les champs</span>";}?>
 		</form>
 	</div>
+
+<script>
+function validateForm() {
+
+}
 <?php include("footer.php")?>
