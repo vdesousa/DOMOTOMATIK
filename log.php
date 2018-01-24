44570f41d20530ded066e27cce711105437120be
lulu780 <34058737+lulu780@users.noreply.github.com>
2018-01-24 23:46:39 +0100

    Correction
    Le log déconne

--- log.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION['id_personne']=2;
-$_SESSION['id_utilisateur']=1;
-echo '<a href=tableaudebord.php>Tableau de bord</a><br/>';
-echo '<a href="vue_ajout_domicile.php">Ajout domicile</a><br/>';
-echo '<a href=espace_client.php>Espace client</a><br/>';
-?>
