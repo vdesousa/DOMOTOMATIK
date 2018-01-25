ee1dff5640b478031cdcd99a7b199d27eeaa8419
Merlin <merlin.rousseau1@gmail.com>
2018-01-25 17:23:27 +0100

    Petite modif 2

--- index1.php
+++ index1.php
@@ -2,11 +2,7 @@
 session_start();
 
 if (isset($_POST['submit'])) {
-  try{
-    $bdd = new PDO('mysql:host=localhost;port=8889;dbname=bdd_5e;charset=utf8', 'root', 'root');
-  }
-  catch (Exception $e){
-    die('Erreur : '.$e->getMessage());
+  include("dbh.php");
   }
 $email = $_POST['email'];
 $code = $_POST['code'];
