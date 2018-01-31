5217b87621643e1b887f4b261b361380cd8cdcf6
Virgilio de Sousa <vacs96@hotmail.com>
2018-01-31 18:11:33 +0100

    ajout de pages admin

--- boutique.php
+++ boutique.php
@@ -1,11 +1,6 @@
 <?php include("headerhc.php") ?>
-<?php   try{
-    $bdd = new PDO('mysql:host=localhost;dbname=bdd_5e;charset=utf8', 'root', 'root');
-  }
-  catch (Exception $e){
-    die('Erreur : '.$e->getMessage());
-  }
-  $req1 = $bdd->query("SELECT `nom_objet`, `photo` FROM `boutique`");
+<?php   include("dbh.php");
+  $req1 = $bdd->query("SELECT `id_objet`, `nom_objet`, `photo` FROM `boutique`");
   $res1 = $req1->fetchAll();
   ?>
 <h1>Catalogue</h1>
@@ -16,8 +11,9 @@
   while ($nbrcapt !=-1) {
       $nomobj = $res1[$nbrcapt]['nom_objet'];
       $photo = $res1[$nbrcapt]['photo'];
-    	echo '<li><a href="'.$nomobj.'php"><img src='.$photo.'><br><span>'.$nomobj.'</span></a></li>';
-      $nbrcapt -=1; }
+      $id = $res1[$nbrcapt]['id_objet'];
+    	echo '<li><a href="description.php?capteur='.$id.'"><img src='.$photo.'.jpg"><br><span>'.$nomobj.'</span></a></li>';
+      $nbrcapt -=1; };
   ?>
 </ul>
 
--- dbh.php
+++ dbh.php
@@ -3,7 +3,8 @@
 /*Connexion BDD*/
 
 try{
-  $bdd = new PDO('mysql:host=localhost;port=8889;dbname=bdd_5e;charset=utf8', 'root', 'root');
+  $bdd = new PDO('mysql:host=localhost;dbname=bdd_5e;charset=utf8', 'root', 'root');
+
 }
 catch (Exception $e){
   die('Erreur : '.$e->getMessage());
