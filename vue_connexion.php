1e9c3954edaac5670d5d006415fa869e0a5a0222
Virgilio de Sousa <vacs96@hotmail.com>
2018-01-31 19:24:19 +0100

    chnage

---
+++ vue_connexion.php
@@ -0,0 +1,18 @@
+<?php include("headerhc.php")?>
+	<?php
+			session_start();
+
+	?>
+	<div class="connexion">
+		<span>Se connecter</span>
+		<form method="post" action="controleur_connexion.php">
+			<label>E-mail:
+			<input type="text" name="email" /></label><br>
+			<label>Mot de Passe:
+			<input type="password" name="password" /></label><?php if (isset($_GET["mdp"]) AND $_GET["mdp"]=="faux") {echo "<span class=erreur>Mot de Passe éronné</span>";} ?><br>
+			<?php if (isset($_GET["champs"]) AND $_GET["champs"]=="vides") {echo "<span class=erreur>Veuillez renseigner tous les champs</span><br>";} ?>
+			<?php if (isset($_GET["utilisateur"]) AND $_GET["utilisateur"]=="inconnu") {echo "<span class=erreur>Votre email est éronné ou vous n'êtes pas inscrit</span><br>";} ?>
+			<input type="submit" name="submit" value="Valider" />
+		</form>
+	</div>
+<?php include("footer.php")?>
