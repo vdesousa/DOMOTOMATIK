a8bcd72cb2309629248b80d130502cd3fb0e24d2
Lenchanteur97 <34679630+Lenchanteur97@users.noreply.github.com>
2018-02-01 20:32:41 +0100

    Add files via upload

---
+++ footer_admin.php
@@ -0,0 +1,11 @@
+</div>
+		<footer>
+       			 <ul>
+            		<li><a href="mentions_legales.php">Mentions légales</a></li>
+            		<li><a href="cgu.php">CGU</a></li>
+            		<li><a href="send_message.php">Contact</a></li>
+            		<li><a href="copyright.php">Copyright</a></li>
+        		</ul>
+    	</footer>
+	</body>
+</html>
