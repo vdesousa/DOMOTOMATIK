2ca2a8d0cda7ef4cd01fd90221279429987b2504
tdangngoc <toan94300@gmail.com>
2018-02-01 22:46:54 +0100

    caca

--- humidite.php
+++
@@ -1,5 +0,0 @@
-<?php include("headerhc.php") ?>
-<h1>Capteur d'humidité</h1>
-<img class="description" src="humidite.jpg">
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-<?php include("footer.php")?>
--- modele_administrateur.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('config.php');
-
-$user=$pdo->query('SELECT * FROM personne INNER JOIN utilisateur ON personne.id_personne = utilisateur.id_utilisateur');
-
-?>
